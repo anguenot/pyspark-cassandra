8abbced33441ae00c2ea1a28016ded3b0f032a93
Julien Anguenot <julien@anguenot.org>
2019-08-06 14:54:32 +0000

    Bump version: 0.11.0 → 2.3.0

--- python/pyspark_cassandra/__init__.py
+++ python/pyspark_cassandra/__init__.py
@@ -58,4 +58,4 @@ for _ in range(10):
 
 __author__ = 'Julien Anguenot'
 __email__ = 'julien@anguenot.org'
-__version__ = '0.11.0'
+__version__ = '2.3.0'
--- python/setup.py
+++ python/setup.py
@@ -24,7 +24,7 @@ setup(
     name='pyspark_cassandra',
     maintainer='Julien Anguenot',
     maintainer_email='julien@anguenot.org',
-    version='0.11.0',
+    version='2.3.0',
     description='Utilities to assist in working with Cassandra and PySpark.',
     long_description="",
     url='https://github.com/anguenot/pyspark-cassandra',
